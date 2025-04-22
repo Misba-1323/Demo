88ab8c9e0627262cb239de197f014478c520d0f7
Misba-1323 <misbakousarrjamadar@gmail.com>
2025-04-22 09:50:16 +0530

    Save Add.java in Main

--- Add.java
+++ Add.java
@@ -5,5 +5,6 @@ public class Add {
         int a = 10 , b = 20;
         System.out.println("Main branch");
         System.out.println("Addition = "+(a+b));
+        System.out.println("Master");
     }
 }
